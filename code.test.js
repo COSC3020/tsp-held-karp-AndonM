c84affead9aeaaa524a24bed71efb7742df91858
Andon Mefford <andonmefford@outlook.com>
2023-11-29 22:04:07 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,21 +3,21 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-dm = [[]];
-assert(tsp_hk(dm) == 0);
+let dm = [[]];
+assert(tspHK(dm) == 0);
 
 dm = [[0]];
-assert(tsp_hk(dm) == 0);
+assert(tspHK(dm) == 0);
 
 dm = [[0,0,0],
       [0,0,0],
       [0,0,0]];
-assert(tsp_hk(dm) == 0);
+assert(tspHK(dm) == 0);
 
 dm = [[0,1,2],
       [1,0,2],
       [2,2,0]];
-assert(tsp_hk(dm) == 3);
+assert(tsphk(dm) == 3);
 
 // https://people.sc.fsu.edu/~jburkardt/datasets/tsp/tsp.html
 dm = [[0,3,4,2,7],
@@ -25,4 +25,4 @@ dm = [[0,3,4,2,7],
       [4,4,0,5,8],
       [2,6,5,0,6],
       [7,3,8,6,0]];
-assert(tsp_hk(dm) == 13);
+assert(tsphk(dm) == 13);
