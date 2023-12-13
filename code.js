1406a44e90f94da17ca9e7a4c7c9999085d3934a
Andon Mefford <andonmefford@outlook.com>
2023-12-13 15:16:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,25 +1,30 @@
 function tspHK(distanceMatrix) {
-    const n = distanceMatrix.length;
-    const memo = new Array(2 ** n).fill().map(() => new Array(n).fill(-1));
-    function heldKarp(visited, pos) {
-      if (visited === (2 ** n) - 1) {
-        return distanceMatrix[pos][0];
-      }
-      if (memo[visited][pos] !== -1) {
-        return memo[visited][pos];
-      }
-      let minDistance = Infinity;
-      for (let next = 0; next < n; next++) {
-        const nextCityBit = 2 ** next;
-        if ((visited & nextCityBit) === 0) {
-          const newVisited = visited | nextCityBit;
-          const newDistance =
-            distanceMatrix[pos][next] + heldKarp(newVisited, next);
-          minDistance = Math.min(minDistance, newDistance);
-        }
-      }
-      memo[visited][pos] = minDistance;
-      return minDistance;
+    if distanceMatrix = [[]] {
+        minDistance == 0;
     }
-    return heldKarp(1, 0);
-  }
+    else {
+        const n = distanceMatrix.length;
+        const memo = new Array(2 ** n).fill().map(() => new Array(n).fill(-1));
+        function heldKarp(visited, pos) {
+          if (visited === (2 ** n) - 1) {
+            return distanceMatrix[pos][0];
+          }
+          if (memo[visited][pos] !== -1) {
+            return memo[visited][pos];
+          }
+          let minDistance = Infinity;
+          for (let next = 0; next < n; next++) {
+            const nextCityBit = 2 ** next;
+            if ((visited & nextCityBit) === 0) {
+              const newVisited = visited | nextCityBit;
+              const newDistance =
+                distanceMatrix[pos][next] + heldKarp(newVisited, next);
+                minDistance = Math.min(minDistance, newDistance);
+                }
+              }
+              memo[visited][pos] = minDistance;
+              return minDistance;
+        }
+        return heldKarp(1, 0);
+    }
+}
