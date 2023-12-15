ff2b8718596108e4d9d0739dfb631ba04d1cfc41
Andon Mefford <andonmefford@outlook.com>
2023-12-14 19:35:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -11,8 +11,7 @@ function tspHK(distanceMatrix) {
         if (visited.length === 2) {
             memo[key] = distanceMatrix[visited[0]][visited[1]];
             return memo[key];
-        } 
-        else {
+        } else {
             let minimum = Infinity;
             for (let i = 0; i < visited.length; i++) {
                 let nextCity = visited[i];
@@ -29,7 +28,7 @@ function tspHK(distanceMatrix) {
     let cities = Array.from({ length: distanceMatrix.length }, (_, i) => i);
     let minTourLength = Infinity;
     for (let start = 1; start < cities.length; start++) {
-        let tourLength = heldKarp([...cities], start) + distanceMatrix[0][start];
+        let tourLength = heldKarp([...cities], start) + distanceMatrix[start][0];
         minTourLength = Math.min(minTourLength, tourLength);
     }
     return minTourLength === Infinity ? 0 : minTourLength;
