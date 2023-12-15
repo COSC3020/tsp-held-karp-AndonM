40e876c5f30af7beebdd178e3aa3f5f42872aa8b
Andon Mefford <andonmefford@outlook.com>
2023-12-14 19:40:44 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -20,9 +20,3 @@ distanceMatrix = [[0,1,2],
 assert(tspHK(distanceMatrix) == 3);
 
 // https://people.sc.fsu.edu/~jburkardt/datasets/tsp/tsp.html
-distanceMatrix = [[0,3,4,2,7],
-      [3,0,4,6,3],
-      [4,4,0,5,8],
-      [2,6,5,0,6],
-      [7,3,8,6,0]];
-assert(tspHK(distanceMatrix) == 13);
